5f2da3c10ea212932681afe1e676b7c5100cf070
Raimon Esteve <raimon@nan-tic.com>
2018-02-27 10:06:25 +0100

    Add use config mangage stock option in sale shop and product

--- __init__.py
+++ __init__.py
@@ -2,9 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-import shop
+from . import product
+from . import shop
+
 
 def register():
     Pool.register(
+        product.Template,
         shop.SaleShop,
         module='magento_stock', type_='model')
